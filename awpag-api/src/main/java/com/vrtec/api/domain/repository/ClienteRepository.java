f476b346019882735115761250423d5f3809137b
Vladimir Rodrigues <rodrigues.vladimir@outlook.com>
2024-03-24 18:13:07 -0300

    CRUD-awpag

---
+++ awpag-api/src/main/java/com/vrtec/api/domain/repository/ClienteRepository.java
@@ -0,0 +1,15 @@
+package com.vrtec.api.domain.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.vrtec.api.domain.model.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+	
+	List<Cliente> findByNomeContaining(String nome);
+
+}
---
+++ src/test/java/com/vrtec/awpag/AwpagApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vrtec.awpag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwpagApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
